8abe8f8e156400b81c1f8a8bd62cc13e0ce32f33
Sunwoo Jung <49130227+InHeritas@users.noreply.github.com>
2025-02-21 06:23:04 +0900

    Continuous corner path

--- Sources/Internal/Extensions/View+Background.swift
+++ Sources/Internal/Extensions/View+Background.swift
@@ -22,42 +22,60 @@ extension View {
 // MARK: Background Shape
 fileprivate struct RoundedCorner: Shape {
     var corners: [PopupAlignment: CGFloat]
-
     
     var animatableData: CGFloat {
         get { corners.values.max() ?? 0 }
-        set { corners.forEach { corners[$0.key] = newValue } }
+        set { corners.keys.forEach { corners[$0] = newValue } }
     }
+    
     func path(in rect: CGRect) -> Path {
-        let points = createPoints(rect)
-        let path = createPath(rect, points)
-        return path
+        let maxRadius = corners.values.max() ?? 0
+        return createContinuousPath(rect, maxRadius)
     }
 }
+
 private extension RoundedCorner {
-    func createPoints(_ rect: CGRect) -> [CGPoint] {[
-        .init(x: rect.minX, y: rect.minY + corners[.top]!),
-        .init(x: rect.minX + corners[.top]!, y: rect.minY),
-        .init(x: rect.maxX - corners[.top]!, y: rect.minY),
-        .init(x: rect.maxX, y: rect.minY + corners[.top]!),
-        .init(x: rect.maxX, y: rect.maxY - corners[.bottom]!),
-        .init(x: rect.maxX - corners[.bottom]!, y: rect.maxY),
-        .init(x: rect.minX + corners[.bottom]!, y: rect.maxY),
-        .init(x: rect.minX, y: rect.maxY - corners[.bottom]!)
-    ]}
-    func createPath(_ rect: CGRect, _ points: [CGPoint]) -> Path { let radius = corners.values.max() ?? 0
+    func createContinuousPath(_ rect: CGRect, _ radius: CGFloat) -> Path {
         var path = Path()
-
-        path.move(to: points[0])
-        path.addArc(tangent1End: CGPoint(x: rect.minX, y: rect.minY), tangent2End: points[1], radius: radius)
-        path.addLine(to: points[2])
-        path.addArc(tangent1End: CGPoint(x: rect.maxX, y: rect.minY), tangent2End: points[3], radius: radius)
-        path.addLine(to: points[4])
-        path.addArc(tangent1End: CGPoint(x: rect.maxX, y: rect.maxY), tangent2End: points[5], radius: radius)
-        path.addLine(to: points[6])
-        path.addArc(tangent1End: CGPoint(x: rect.minX, y: rect.maxY), tangent2End: points[7], radius: radius)
+        
+        let topLeft = corners[.top] ?? 0
+        let topRight = corners[.top] ?? 0
+        let bottomRight = corners[.bottom] ?? 0
+        let bottomLeft = corners[.bottom] ?? 0
+        
+        path.move(to: CGPoint(x: rect.minX + topLeft, y: rect.minY))
+        
+        // Top Left
+        path.addQuadCurve(
+            to: CGPoint(x: rect.minX, y: rect.minY + topLeft),
+            control: CGPoint(x: rect.minX, y: rect.minY)
+        )
+        
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY - bottomLeft))
+        
+        // Bottom Left
+        path.addQuadCurve(
+            to: CGPoint(x: rect.minX + bottomLeft, y: rect.maxY),
+            control: CGPoint(x: rect.minX, y: rect.maxY)
+        )
+        
+        path.addLine(to: CGPoint(x: rect.maxX - bottomRight, y: rect.maxY))
+        
+        // Bottom Right
+        path.addQuadCurve(
+            to: CGPoint(x: rect.maxX, y: rect.maxY - bottomRight),
+            control: CGPoint(x: rect.maxX, y: rect.maxY)
+        )
+        
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY + topRight))
+        
+        // Top Right
+        path.addQuadCurve(
+            to: CGPoint(x: rect.maxX - topRight, y: rect.minY),
+            control: CGPoint(x: rect.maxX, y: rect.minY)
+        )
+        
         path.closeSubpath()
-
         return path
     }
 }
